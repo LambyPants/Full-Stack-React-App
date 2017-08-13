85c157dfde647bd5b27c3b692dac89645294857a
Ryan Lambert <ryanlambert@Ryans-iMac.local>
2017-08-13 10:56:29 -0500

    add env variables and prod database

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//production keys go here, you can commit this file no probbies
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
