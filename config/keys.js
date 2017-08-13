1dc58ef3fe3a4df65266cb88048e3b4176b28c11
Ryan Lambert <ryanlambert@Ryans-iMac.local>
2017-08-13 11:00:56 -0500

    add log to keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 //keys js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
+  console.log('production');
   //we are in production return prod keys
   module.exports = require('./prod');
 } else {
