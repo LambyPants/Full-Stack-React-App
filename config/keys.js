6ceb5593ab5d6f13c474da73c02a1095d7510535
Ryan Lambert <ryanlambert@Ryans-iMac.local>
2017-08-14 20:06:50 -0500

    add async refactor

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  console.log('production');
+  //we are in production return prod keys
+  module.exports = require('./prod');
+} else {
+  //we are in dev return dev keys
+  module.exports = require('./dev');
+}
