9736522584ca53df041350ffa17785ae90f18957
Ryan Lambert <ryanlandonlambert@gmail.com>
2017-10-22 18:24:53 -0500

    prep for prod heroku commit

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+if(process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets
+  //Like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+  //Express will serve up index.html if route unrecognized
+  const path = require('path');
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
    console.log('server started - you are my human overlord');
